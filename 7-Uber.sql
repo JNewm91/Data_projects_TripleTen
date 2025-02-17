1bb79123c2dd9ad712c07f69637f861b4ffb92f9
Joseph Newman <jm.newman.kc@gmail.com>
2025-02-16 22:02:18 -0600

    Update 7-Uber.sql

--- 7-Uber.sql
+++ 7-Uber.sql
@@ -22,7 +22,9 @@
         -- record_id: weather record code
         -- ts: record date and time (time rounded to the hour)
         -- temperature: temperature when the record was taken
-        -- description: brief description of weather conditions, e.g. "light rain" or "scattered clouds"
+        -- description: a brief description of weather conditions, e.g. "light rain" or "scattered clouds"
+
+
 -- The code below is used to find the number of taxi rides for each taxi company for November 15-16, 2017, and name the resulting field trips_amount, then print it. The results are sorted by the trips_amount field in descending order.
 
 SELECT 
@@ -39,6 +41,7 @@ GROUP BY
     company_name 
 ORDER BY 
     trips_amount DESC;
+
 
 -- The code below is used to find the number of rides for every taxi company whose name contains the words "Yellow" or "Blue" for November 1-7, 2017. The resulting variable is named trips_amount and the results are grouped by the company_name field.
 
@@ -60,6 +63,32 @@ ORDER BY
     trips_amount;
 
 
+-- The code below is used to find the number of rides for the two most popular taxi companies: 'Flash Cab' and 'Taxi Affiliation Services'. The resulting variable was named 'trips_amount'. The rides for all other companies were joined in the group 'Other'. The data was then grouped by taxi company names and named 'company', with the results sorted in descending order by 'trips_amount'.
+
+SELECT 
+    company,
+    COUNT(trip_id) AS trips_amount
+FROM (
+    SELECT 
+        CASE 
+            WHEN company_name IN 
+            ('Flash Cab', 'Taxi Affiliation Services')
+            THEN company_name 
+            ELSE 'Other'
+        END AS company,
+        trip_id
+    FROM 
+        cabs
+    JOIN trips ON 
+        cabs.cab_id = trips.cab_id 
+    WHERE 
+        start_ts >= '2017-11-01' 
+        AND start_ts < '2017-11-08'
+) AS companies 
+GROUP BY 
+    company 
+ORDER BY 
+    trips_amount DESC;
 
 
 
